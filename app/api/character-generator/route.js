b436739c958a6b689f0dc4d92235f40c428c6219
Victor-MT <victor.martins1699@gmail.com>
2026-05-14 17:46:26 -0300

    design: fix design issues

--- app/api/character-generator/route.js
+++ app/api/character-generator/route.js
@@ -12,9 +12,7 @@ export async function POST(req) {
     }
 
     const { prompt } = await req.json();
-    
-    console.log("Prompt recebido:", prompt);
-    
+      
     const response = await fetch(
       `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash-lite:generateContent?key=${process.env.GEMINI_API_KEY}`,
       {
